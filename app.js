ea2afec4cf587b515679ca4a36f92823ed2563bd
Jorge Ramos <jorge.ramos@assertus.com>
2018-12-03 16:54:00 -0400

    update

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 var express = require('express');
 var app = express();
-
+var $ = require('jQuery');
+const fs = require('fs');
+var bodyParser = require('body-parser')
 
 app.use(express.static('public'));
 app.use(express.static('.'))
-
+app.use(express.static('node_modules'))
 // app.get('/', function (req, res) {
 //   res.send('./index.html');
 // });
-
+app.use(bodyParser.json())
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+app.get('/pointSave', function (req, res) {
+		let pointsdata = fs.readFileSync('points.json');  
+		let points = JSON.parse(pointsdata);   
+		
+		res.send(points)
+})
 
+app.post('/pointSave', function (req, res) {
+	let points = req.body
+	var data = new Object();
+	data.points = points;
+	var toFile = JSON.stringify(data); 
+	fs.writeFile('points.json',toFile , (err) => {  
+    if (err) throw err;
+    console.log('Data written to file');
+    console.log(toFile);
+	});
+	
+  res.send("req")
+})
+
+// let student = {  
+//     name: 'Mike',
+//     age: 23, 
+//     gender: 'Male',
+//     department: 'English',
+//     car: 'Honda' 
+// };
+
+// let data = JSON.stringify(student, null, 2);
+
+// fs.writeFile('points.json', data, (err) => {  
+//     if (err) throw err;
+//     console.log('Data written to file');
+// });
+
+// console.log('This is after the write call');  
--- public/js/app.js
+++ public/js/app.js
@@ -1,86 +1,203 @@
-window.onload = function() {
-
-        var _height = 850;
-        var _width = 750;
-        var scene = sjs.Scene({w:_width, h:_height});
-
-        var layer1 = scene.Layer('background' );
-        
-        var options = { layer:layer1, size:[1300,1300]};
-        var sprite = scene.Sprite("img/body_s.png", options);
-        scene.loadImages(['img/x.png'])
-        //sprite.scale(.1);
-        sprite.move(0, 0);
-        sprite.update();
-        scene.dom.addEventListener('click', function(e) {
-            console.log(e)
-
-             let options1 = { layer:layer1,  x:1,y:1};
-            // 
-            
-            let sprite1 = scene.Sprite("img/x.png",options1);
-            sprite1.move(e.layerX-5,e.layerY-5);
-            sprite1.scale(.1,.1);
-            console.log( sprite1);
-            sprite1.update();
-        }, true);
-
-//     scene.loadImages(['img/body_s.jpg'], function() {
-
-//         var function_array = [
-//             {'name': 'Initialization',
-//                 'execute':function () {
-// bird = scene.Sprite('img/body_s.jpg');
-// bird.update();
-//             }},
-//             {'name': 'Set position',
-//                 'execute':function () {
-// bird.position(200, 200);
-// bird.update();
-//             }},
-//             {'name': 'Move',
-//                 'execute':function () {
-// bird.move(-10, -10);
-// bird.update();
-//             }},
-//             {'name': 'Rotate',
-//                 'execute':function () {
-// bird.rotate(Math.PI / 3.0);
-// bird.update();
-//             }},
-//             {'name': 'Scale',
-//                 'execute':function () {
-// bird.scale(bird.xscale + 0.5, bird.yscale + 0.5);
-// bird.update();
-//             }},
-//             {'name': 'Set opacity',
-//                 'execute':function () {
-// bird.setOpacity(bird.opacity - 0.2);
-// bird.update();
-//             }},
-//         ]
-
-//         var list_str = "";
-//         for(var i=0; i<function_array.length; i++) {
-//             var el = function_array[i];
-//             var code = String(el['execute']).split('{')[1].split('}')[0].replace(/^\s+|\s+$/g, '');
-
-//             list_str = list_str + '<li><h2>'+ el.name
-//               +'</h2><pre>'+
-//               code
-//               +'</pre><button id="exec_'+
-
-//               i+'">Execute</button></li>';
-
-//         }
-
-//         exec_list.innerHTML = list_str;
-//         exec_list.onclick = function(e) {
-//             var index = parseInt(e.target.id.split('_')[1]);
-//             if(function_array[index])
-//                 function_array[index].execute();
-//         }
-
-//     });
-
+window.onload = async function() {
+    const {
+        Scene,
+        Sprite,
+        Group,
+        Label
+    } = spritejs;
+    const scene = new Scene('#paper', {
+        viewport: ['auto', 'auto'],
+        resolution: [724, 201]
+    });
+    var pointsList = new Array();
+    // await scene.preload([
+    //    'img/body_s.png'
+    //  ]);
+    // await scene.preload([
+    //    'img/x.png'
+    //  ]);
+    const layer = scene.layer('layer');
+    //layer.canvas.style.backgroundColor = '#FFFDCC';
+    const body_s = new Sprite();
+    body_s.attr({
+        anchor: [0, 0],
+        textures: 'img/body_s.png'
+    });
+    layer.append(body_s);
+    layer.on('click', function(e) {
+        console.log(e)
+        if (e.target.text == null) {
+            if (e.target.texturesSize[0] > 200) {
+                let sprite1 = new Sprite("img/x.png");
+                sprite1.attr({
+                    x: e.layerX - 10,
+                    y: e.layerY - 10,
+                    scale: [0.10, 0.10]
+                });
+                sprite1.on('click', function(e) {
+                    layer.remove(sprite1);
+                })
+                //sprite1.scale(.05,.05);
+                console.log(sprite1);
+                layer.append(sprite1);
+                pointsList.push({
+                    "x": (e.layerX - 10),
+                    "y": (e.layerY - 10)
+                })
+            }
+        }
+        if (e.target.text == "Save") {};
+    });
+    let resp = await $.getJSON("points.json", function(json) {
+        for (var item of json.points) {
+            //console.log(json); // this will show the info it in firebug console
+            let sprite1 = new Sprite("img/x.png");
+            sprite1.attr({
+                x: item.x,
+                y: item.y,
+                scale: [0.10, 0.10]
+            });
+            sprite1.on('click', function(e) {
+                layer.remove(sprite1);
+            })
+            pointsList.push({
+                    "x": (item.x ),
+                    "y": (item.y )
+                })
+            //sprite1.scale(.05,.05);
+            console.log(sprite1);
+            layer.append(sprite1);
+        }
+    });
+    //*****************************************************
+    const button = new Group();
+    button.attr({
+        anchor: 0.5,
+        pos: [660, 180],
+    });
+    layer.append(button);
+    const buttonNormal = new Label('Save');
+    buttonNormal.attr({
+        font: '10px "Arial"',
+        fillColor: '#04773B',
+        lineHeight: 30,
+        textAlign: 'center',
+        size: [50, 30],
+        border: [3, '#178C4E'],
+        borderRadius: 48,
+    });
+    button.append(buttonNormal);
+    const buttonHover = new Sprite();
+    buttonHover.attr({
+        bgcolor: '#208B50',
+        height: 35,
+        width: 40,
+        opacity: 0,
+        borderRadius: 30,
+        zIndex: -1,
+    });
+    button.append(buttonHover);
+    /* eslint-disable no-console */
+    console.log('Place mouse hover the button...');
+    /* eslint-enable no-console */
+    let hoverAnim = null;
+    let touched = false;
+    // prevent mouseenter on mobile device
+    // mobile point events: touchstart > (touchmove)+ > touchend > (mouseenter) > mouseover > mousemove > mousedown > mouseup > click
+    // https://patrickhlauke.github.io/touch/tests/results/
+    button.on('mouseenter', async (evt) => {
+        if (touched) return;
+        layer.canvas.style.cursor = 'pointer';
+        buttonNormal.attr({
+            fillColor: '#fff',
+        });
+        if (hoverAnim) {
+            hoverAnim.cancel();
+            hoverAnim = null;
+        }
+        hoverAnim = buttonHover.animate([{
+            width: 30,
+            opacity: 0
+        }, {
+            width: 55,
+            opacity: 1
+        }, ], {
+            duration: 300,
+            fill: 'forwards',
+            easing: 'ease-in',
+        });
+        await hoverAnim.finished;
+        hoverAnim = null;
+    }).on('mouseleave', async (evt) => {
+        if (touched) return;
+        layer.canvas.style.cursor = 'default';
+        buttonNormal.attr({
+            fillColor: '#04773B',
+        });
+        if (hoverAnim) {
+            hoverAnim.cancel();
+            hoverAnim = null;
+        }
+        hoverAnim = buttonHover.animate([{
+            width: 30,
+            opacity: 1
+        }, {
+            width: 55,
+            opacity: 0
+        }, ], {
+            duration: 500,
+            fill: 'forwards',
+            easing: 'ease-out',
+        });
+        await hoverAnim.finished;
+        hoverAnim = null;
+    });
+    button.on('touchstart', (evt) => {
+        touched = true;
+        if (hoverAnim) {
+            hoverAnim.cancel();
+            hoverAnim = null;
+        }
+        buttonNormal.attr({
+            fillColor: '#fff',
+        });
+        buttonHover.attr({
+            width: 50,
+        });
+    });
+    button.on('touchend', (evt) => {
+        if (hoverAnim) {
+            hoverAnim.cancel();
+            hoverAnim = null;
+        }
+        buttonNormal.attr({
+            fillColor: '#04773B',
+        });
+        buttonHover.attr({
+            width: 0,
+        });
+    });
+    button.on('mousedown', (evt) => {
+        button.attr({
+            scale: 0.9,
+        });
+    }).on('mouseup', (evt) => {
+        button.attr({
+            scale: 1.0,
+        });
+    });
+    button.on('click', (evt) => {
+        /* eslint-disable no-console */
+        console.log('button clicked');
+        $.ajax("/pointSave", {
+            data: JSON.stringify(pointsList),
+            contentType: 'application/json',
+            type: 'POST'
+        }).done(function(data) {
+            var dataURL = layer.canvas.toDataURL('png');
+            console.log(dataURL);
+            var print_canvas = document.getElementById("print_canvas");
+            print_canvas.src = dataURL;
+        });
+    })
 };
